b9bf545969a528eb9808ceab20409522f2b17e30
yangyue <yy@moz0.com>
2019-08-02 18:01:36 +0800

    add websocket

--- go/go.mod
+++ go/go.mod
@@ -3,4 +3,5 @@ module go
 require (
 	github.com/SherClockHolmes/webpush-go v1.1.0
 	github.com/go-sql-driver/mysql v1.4.1
+	github.com/gorilla/websocket v1.4.0
 )
--- go/index.go
+++ go/index.go
@@ -1,69 +1,15 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
+	"net/http"
 
 	"go/utils"
-
-	_ "github.com/go-sql-driver/mysql"
+	"go/websocket"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:123456@tcp(192.168.99.100:3306)/user?charset=utf8")
+	http.HandleFunc("/", websocket.Handler)
+	// err := http.ListenAndServe(":6503", nil)
+	err := http.ListenAndServeTLS(":6503", "./config/gd_bundle-g2-g1.crt", "./config/generated-private-key.txt", nil)
 	utils.PanicErr(err)
-
-	// insert
-	stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,created=?")
-	utils.PanicErr(err)
-
-	res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
-	utils.PanicErr(err)
-
-	id, err := res.LastInsertId()
-	utils.PanicErr(err)
-
-	fmt.Println(id)
-	// update
-	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
-	utils.PanicErr(err)
-
-	res, err = stmt.Exec("astaxieupdate", id)
-	utils.PanicErr(err)
-
-	affect, err := res.RowsAffected()
-	utils.PanicErr(err)
-
-	fmt.Println(affect)
-
-	// query
-	rows, err := db.Query("SELECT * FROM userinfo")
-	utils.PanicErr(err)
-
-	for rows.Next() {
-		var uid int
-		var username string
-		var department string
-		var created string
-		err = rows.Scan(&uid, &username, &department, &created)
-		utils.PanicErr(err)
-		fmt.Println(uid)
-		fmt.Println(username)
-		fmt.Println(department)
-		fmt.Println(created)
-	}
-
-	// delete
-	stmt, err = db.Prepare("delete from userinfo where uid=?")
-	utils.PanicErr(err)
-
-	res, err = stmt.Exec(id)
-	utils.PanicErr(err)
-
-	affect, err = res.RowsAffected()
-	utils.PanicErr(err)
-
-	fmt.Println(affect)
-
-	db.Close()
 }
