a301f32c7321678ab94f0a0c6741362b7a162273
yangyue <yy@moz0.com>
2019-08-08 16:17:41 +0800

    Updates

---
+++ precache-manifest.03875a2d03d562cb3fdd261ba237ca8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7262aa693313d6e8dd54e55eafa68196",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50e6b82d20f269079079",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a563b205b66034e5b5c3",
+    "url": "/static/js/2.6108dce8.chunk.js"
+  },
+  {
+    "revision": "50e6b82d20f269079079",
+    "url": "/static/js/main.4a1330a9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
