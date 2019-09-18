5bdb5dc03a3ee7352f0a4bd6d995f1ebb2dd2b07
yangyue <yy@moz0.com>
2019-09-18 12:06:48 +0800

    build docker-compose

---
+++ white/build/precache-manifest.bac0816a2ec6cfd05f47daa29b7feb98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8e58fa6ba32809f2442b15f91a0b52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38661e06e9dde7ab0456",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b799970bc9b0dcc5f409",
+    "url": "/static/js/2.07237b8c.chunk.js"
+  },
+  {
+    "revision": "38661e06e9dde7ab0456",
+    "url": "/static/js/main.4f51c908.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
