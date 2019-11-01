ed79ef029f2a408c06afd22e2f66df4af5531bc7
yy <yy@moz0.com>
2019-11-01 22:04:14 +0800

    fix database config

--- go/crawler/main.go
+++ go/crawler/main.go
@@ -15,12 +15,12 @@ func main() {
 		ItemSaver:   itemsaver.New(),
 	}
 	request := engine.Request{
-		// URL: "https://cd.lianjia.com/ershoufang/",
-		// Parser: parser.City,
-		URL: "https://cd.lianjia.com/ershoufang/106103655131.html",
-		Parser: func(q *goquery.Document) engine.Result {
-			return parser.House(q, "https://cd.lianjia.com/ershoufang/106103655131.html")
-		},
+		URL: "https://cd.lianjia.com/ershoufang/",
+		Parser: parser.City,
+		// URL: "https://cd.lianjia.com/ershoufang/106103655131.html",
+		// Parser: func(q *goquery.Document) engine.Result {
+		// 	return parser.House(q, "https://cd.lianjia.com/ershoufang/106103655131.html")
+		// },
 	}
 	entry.Run(request)
 }
