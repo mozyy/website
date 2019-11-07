f88cf57e1fc6a921d98c99f5016bc8fd111fe756
yangyue <yy@moz0.com>
2019-11-07 15:33:29 +0800

    other regexp file function

--- python.yyue.dev/zq/main.py
+++ python.yyue.dev/zq/main.py
@@ -3,29 +3,34 @@ import json
 filename='./sources/贝森虚拟服务器IP地址.log'
 
 ips = []  # 所有结果写入的数组
-file = open(filename, encoding='utf-8')
-constent = file.read()
-file.close() # 文件用完, 立即关闭, 防止内存泄漏
 
-# 正则表达式, 提前编译, 提升效率
-pattern = re.compile('^slb virtual-server \S+ (\d+\.\d+\.\d+\.\d+).*?((?=port)[^!]*)!$', re.M | re.S)
-subPattern = re.compile('port (\d+).*?service-group (\S+)', re.M | re.S)
-
-matchs = pattern.findall(constent) # 查找所有的 slb 到 ! 的内容
-
-# print(matchs)
-
-for match in matchs: # 遍历所有匹配结果
-	ip = {'ip': match[0], 'service-groups':[]} # 生成一个结果 字典, service-groups 数组用于存放服务组信息
-	subMatchs = subPattern.findall(match[1]) # 匹配 port 和 service-group
-	for subMatch in subMatchs:
-		ip['service-groups'].append({  # 在结果字典的service-groups 里插入port 和service-group
-			'port': subMatch[0],
-			'service-group': subMatch[1], 
-		})
-	ips.append(ip) # 把ip 插入ips
+with open(filename, encoding='utf-8') as file:
+	while True:
+		line = file.readline()
+		if line:
+			if line.find('slb')>-1:
+				ip = {'ip':line.split(' ')[3],'service-groups':[]}
+				while True:
+					line = file.readline().strip()
+					group = {}
+					if line != '!':
+						if line.find('port') > -1:
+							group['port'] = line.split(' ')[1]
+							while True:
+								line = file.readline()
+								line = line.strip()
+								if line.find('service-group') > -1:
+									# print(line)
+									group['service-group'] = line.split(' ')[1]
+									ip['service-groups'].append(group)
+									break
+					else:
+						break
+				ips.append(ip)
+		else:
+			break
 
 resultStr = json.dumps(ips, sort_keys=True, indent=2) # 把结果转为json字符串, 便于查看
 
-resultFile = open('./result.txt','w')
+resultFile = open('./result2.txt','w')
 resultFile.write(resultStr)
